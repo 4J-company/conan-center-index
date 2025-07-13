b148504ef8ed4c63236d1b5e28ff6713a4ad3cc9
Michael Tsukanov <tsukanovWOW@ya.ru>
2025-07-13 03:43:41 +0300

    Add mr-manager@1.0.1 package

--- recipes/mr-manager/all/conanfile.py
+++ recipes/mr-manager/all/conanfile.py
@@ -1,11 +1,17 @@
+import os
+
 from conan import ConanFile
-from conan.tools.layout import basic_layout
+from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMakeDeps
+from conan.tools.cmake import CMakeToolchain
+from conan.tools.cmake import CMake
 from conan.tools.build import check_min_cppstd
-from conan.tools.files import copy
+from conan.tools.files import copy, rmdir, get
+from conan.api.conan_api import ConanAPI
 
 class MrManager(ConanFile):
     name = "mr-manager"
-    version = "1.0"
+    version = "1.0.1"
     license = "MIT"
 
     description = "Wait-Free object manager with per-type memory pools"
@@ -15,29 +21,42 @@ class MrManager(ConanFile):
 
     settings = "os", "compiler", "build_type", "arch"
 
-    exports_sources = "CMakeLists.txt", "include/*"
-
     package_type = "header-library"
+
     implements = ["auto_header_only"]
 
     def validate(self):
+        if not any(remote.name.lower() == "4j-company" for remote in ConanAPI().remotes.list()):
+            raise Exception("Required remote '4J-company' is missing. Please add it manually. For details, see https://github.com/4J-company/conan-center-index")
+
+        check_min_cppstd(self, "20")
         check_min_cppstd(self, "23")
 
     def requirements(self):
         self.requires("folly/2024.08.12.00")
 
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.27]")
+
     def source(self):
-        git = Git(self)
-        git.clone("https://github.com/4J-company/mr-manager.git", ".")
-        git.checkout("4fad70eb1143f2b4c7cc794f5afc217d5f44cb34")
-  
+        get(self, **self.conan_data["sources"][self.version])
+
+    def generate(self):
+        toolchain = CMakeToolchain(self)
+        toolchain.presets_prefix = "mr-manager"
+        toolchain.generate()
+
     def layout(self):
-        basic_layout(self)
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def package(self):
-        copy(self, "include/*", self.source_folder, self.package_folder)
-
-    def package_info(self):
-        self.cpp_info.bindirs = []
-        self.cpp_info.libdirs = []
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
