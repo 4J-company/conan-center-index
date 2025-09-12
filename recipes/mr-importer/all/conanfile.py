88736d62ee790460fe535a8f818a7e2037eedf70
Michael Tsukanov <tsukanovWOW@ya.ru>
2025-09-13 01:18:01 +0300

    Add mr-importer@1.2.2 package

--- recipes/mr-importer/all/conanfile.py
+++ recipes/mr-importer/all/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.files import copy, rmdir, get
 
 class mr_importerRecipe(ConanFile):
     name = "mr-importer"
-    version = "1.2.1"
+    version = "1.2.2"
     package_type = "library"
 
     license = "MIT"
